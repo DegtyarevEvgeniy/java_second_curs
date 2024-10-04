775d156d85477e52828671e16bee639c7d6cf22c
DegtyarevEvgeniy <56451979+DegtyarevEvgeniy@users.noreply.github.com>
2024-10-04 13:27:19 +0300

    Create Main.java

---
+++ prak4/src/Main.java
@@ -0,0 +1,16 @@
+import vehicles.ElectricCar;
+import vehicles.Vehicle;
+import vehicles.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car common_car=new Car("uazik", "22814x", "red", 2013, "Sam Stone", 1226533, "Classic");
+        System.out.println(common_car.vehicleType());
+        common_car.setModel("Serious Uazik 3000");
+        System.out.println(common_car.toString());
+        ElectricCar common_elcar= new ElectricCar("tesla 228", "usa govna", "black", 2077, "Ilon M", 1212231, "Electric", 220);
+        System.out.println(common_elcar.vehicleType());
+        common_elcar.setBatteryCapacity(120);
+        System.out.println(common_elcar.toString());
+    }
+}
